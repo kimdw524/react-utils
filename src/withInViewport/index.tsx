ec2f589dce7143e0cc3bc1304cc57fc3ee6ef67d
kimdw524 <kimdw524@naver.com>
2025-01-27 00:55:00 +0900

    feat: withInViewport

---
+++ src/withInViewport/index.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { ComponentType, useMemo, useRef } from 'react';
+
+import useIsInViewport from '@/hooks/useIsInViewport';
+
+const withInViewport = <T extends { ref?: React.RefObject<HTMLElement | null> }>(
+  WrapperComponent: ComponentType<T>,
+) => {
+  const InViewportComponent = (props: T) => {
+    const ref = useRef<HTMLElement>(null);
+    const { isInViewport } = useIsInViewport(props.ref || ref);
+
+    const memo = useMemo(() => {
+      if (isInViewport) {
+        return;
+      }
+
+      return <WrapperComponent ref={ref} {...props} />;
+    }, [isInViewport]);
+
+    if (!isInViewport) {
+      return memo;
+    }
+
+    return <WrapperComponent ref={ref} {...props} />;
+  };
+
+  return InViewportComponent;
+};
+
+export default withInViewport;
