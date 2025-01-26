0bf5e7ef235eb0aaa9c93f136ca731b02a7feabe
kimdw524 <kimdw524@naver.com>
2025-01-27 00:53:11 +0900

    feat: useIsInViewport

---
+++ src/useIsInViewport/index.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+
+const useIsInViewport = <T extends HTMLElement | null>(targetRef: React.RefObject<T>) => {
+  const [isInViewport, setIsInViewport] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!targetRef.current) {
+      return;
+    }
+
+    const observerCallback = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach(entry => {
+        setIsInViewport(entry.isIntersecting);
+      });
+    };
+
+    const observer = new window.IntersectionObserver(observerCallback, {
+      threshold: 0,
+    });
+
+    observer.observe(targetRef.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [targetRef]);
+
+  return { isInViewport };
+};
+
+export default useIsInViewport;
