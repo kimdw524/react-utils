19bf2f11796629c7001c5ee372fe59fabf885d00
kimdw524 <kimdw524@naver.com>
2025-01-28 04:01:09 +0900

    fix: toBeInTheDocument type error 해결

--- src/useOverlay/useOverlay.spec.tsx
+++ src/useOverlay/useOverlay.spec.tsx
@@ -1,6 +1,9 @@
+import React from 'react';
+
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import React from 'react';
+
 import { OverlayProvider, useOverlay } from '..';
 
 describe('useOverlay', () => {
